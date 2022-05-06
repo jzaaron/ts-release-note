0be122f01dcd9236a1be10841615e922680b8306
jzaaron <jzystdtm@gmail.com>
2022-05-06 08:00:22 +0800

    init

---
+++ demo/v39.ts
@@ -0,0 +1,31 @@
+/**
+ * ts-expect-error
+ */
+
+function calc(num: number) {
+    console.log(1 + num);
+}
+
+// @ts-expect-error
+calc('1');
+
+/**
+ * getters-setters-are-no-longer-enumerable
+ */
+class A {
+    get words() {
+        return 'hello!'
+    }
+}
+
+const a = new A();
+
+console.log(Object.getOwnPropertyDescriptor(A.prototype, 'words'));
+
+/**
+ * type-parameters-that-extend-any-no-longer-act-as-any.ts
+ */
+
+function fn<T extends any>(t: T) {
+    console.log(t.props)
+}
